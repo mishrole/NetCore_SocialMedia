ff05abbb1240eb58056111bef2c4aa42208b9420
Mitchell Rodríguez <mitrodle@gmail.com>
2021-07-22 12:40:28 -0500

    [Add]: Clean Architecture

---
+++ SocialMedia.Core/Entities/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SocialMedia.Core.Entities
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public DateTime Date { get; set; }
+        public String Description { get; set; }
+        public String Image { get; set; }
+
+    }
+}
---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,24 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(p => new Post
+            {
+                PostId = p,
+                Description = $"Description {p}",
+                Date = DateTime.Now,
+                Image = $"https://miaspis.com/{p}",
+                UserId = p + 1
+            });
+
+            return posts;
+        }
+    }
+}
