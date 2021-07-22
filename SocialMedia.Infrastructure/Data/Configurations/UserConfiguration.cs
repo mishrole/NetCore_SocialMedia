de9525002f8e47d7b559d167723926600b1a5680
Mitchell Rodríguez <mitrodle@gmail.com>
2021-07-22 17:33:24 -0500

    [Add]: Fluent API Configuration

---
+++ SocialMedia.Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMedia.Core.Entities;
+
+namespace SocialMedia.Infrastructure.Data.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Usuario");
+
+            builder.HasKey(e => e.UserId);
+
+            builder.Property(e => e.UserId)
+                .HasColumnName("IdUsuario")
+                .ValueGeneratedNever();
+
+            builder.Property(e => e.LastName)
+                .IsRequired()
+                .HasColumnName("Apellidos")
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            builder.Property(e => e.Email)
+                .IsRequired()
+                .HasMaxLength(30)
+                .IsUnicode(false);
+
+            builder.Property(e => e.DateOfBirth)
+                .HasColumnName("FechaNacimiento")
+                .HasColumnType("date");
+
+            builder.Property(e => e.FirstName)
+                .IsRequired()
+                .HasColumnName("Nombres")
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            builder.Property(e => e.Telephone)
+                .HasColumnName("Telefono")
+                .HasMaxLength(10)
+                .IsUnicode(false);
+
+            builder.Property(e => e.IsActive)
+                .HasColumnName("Activo")
+                .ValueGeneratedNever();
+        }
+    }
+}
--- SocialMedia.Infrastructure/Data/SocialMediaContext.cs
+++ SocialMedia.Infrastructure/Data/SocialMediaContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SocialMedia.Core.Entities;
+using SocialMedia.Infrastructure.Data.Configurations;
 
 namespace SocialMedia.Infrastructure.Data
 {
@@ -22,129 +23,14 @@ namespace SocialMedia.Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Comment>(entity =>
-            {
-                entity.ToTable("Comentario");
+            //modelBuilder.Entity<Comment>(entity => { });
+            modelBuilder.ApplyConfiguration(new CommentConfiguration());
 
-                entity.HasKey(e => e.CommentId);
+            //modelBuilder.Entity<Post>(entity => {});
+            modelBuilder.ApplyConfiguration(new PostConfiguration());
 
-                entity.Property(e => e.CommentId)
-                    .HasColumnName("IdComentario")
-                    .ValueGeneratedNever();
-
-                entity.Property(e => e.PostId)
-                    .HasColumnName("IdPublicacion")
-                    .ValueGeneratedNever();
-
-                entity.Property(e => e.UserId)
-                    .HasColumnName("IdUsuario")
-                    .ValueGeneratedNever();
-
-                entity.Property(e => e.IsActive)
-                    .HasColumnName("Activo")
-                    .ValueGeneratedNever();
-
-                entity.Property(e => e.Description)
-                    .IsRequired()
-                    .HasColumnName("Descripcion")
-                    .HasMaxLength(500)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Date)
-                    .HasColumnName("Fecha")
-                    .HasColumnType("datetime");
-
-                entity.HasOne(d => d.Post)
-                    .WithMany(p => p.Comments)
-                    .HasForeignKey(d => d.PostId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Comentario_Publicacion");
-
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.Comments)
-                    .HasForeignKey(d => d.UserId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Comentario_Usuario");
-            });
-
-            modelBuilder.Entity<Post>(entity =>
-            {
-
-                entity.ToTable("Publicacion");
-
-                entity.HasKey(e => e.PostId);
-
-                entity.Property(e => e.PostId)
-                    .HasColumnName("IdPublicacion")
-                    .ValueGeneratedNever();
-
-                entity.Property(e => e.UserId)
-                    .HasColumnName("IdUsuario")
-                    .ValueGeneratedNever();
-
-                entity.Property(e => e.Description)
-                    .IsRequired()
-                    .HasColumnName("Descripcion")
-                    .HasMaxLength(1000)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Date)
-                    .HasColumnName("Fecha")
-                    .HasColumnType("datetime");
-
-                entity.Property(e => e.Image)
-                    .HasColumnName("Imagen")
-                    .HasMaxLength(500)
-                    .IsUnicode(false);
-
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.Posts)
-                    .HasForeignKey(d => d.UserId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Publicacion_Usuario");
-            });
-
-            modelBuilder.Entity<User>(entity =>
-            {
-
-                entity.ToTable("Usuario");
-
-                entity.HasKey(e => e.UserId);
-
-                entity.Property(e => e.UserId)
-                    .HasColumnName("IdUsuario")
-                    .ValueGeneratedNever();
-
-                entity.Property(e => e.LastName)
-                    .IsRequired()
-                    .HasColumnName("Apellidos")
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Email)
-                    .IsRequired()
-                    .HasMaxLength(30)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.DateOfBirth)
-                    .HasColumnName("FechaNacimiento")
-                    .HasColumnType("date");
-
-                entity.Property(e => e.FirstName)
-                    .IsRequired()
-                    .HasColumnName("Nombres")
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Telephone)
-                    .HasColumnName("Telefono")
-                    .HasMaxLength(10)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.IsActive)
-                    .HasColumnName("Activo")
-                    .ValueGeneratedNever();
-            });
+            //modelBuilder.Entity<User>(entity => { });
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
 
             // OnModelCreatingPartial(modelBuilder);
         }
