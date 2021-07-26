0341b2df6f45fb620af107780eed24b94e93d7e1
Mitchell Rodríguez <mitrodle@gmail.com>
2021-07-25 23:09:52 -0500

    [Add]: Pagination Data on Custom Header

--- SocialMedia.API/Controllers/PostController.cs
+++ SocialMedia.API/Controllers/PostController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using SocialMedia.API.Responses;
 using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
@@ -31,6 +32,18 @@ namespace SocialMedia.API.Controllers
             var posts = _postService.GetPosts(filters);
             var postsDtos = _mapper.Map<IEnumerable<PostDto>>(posts);
             var response = new ApiResponse<IEnumerable<PostDto>>(postsDtos);
+
+            var metadata = new
+            {
+                posts.TotalCount,
+                posts.PageSize,
+                posts.CurrentPage,
+                posts.TotalPages,
+                posts.HasNextPage,
+                posts.HasPreviousPage
+            };
+
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(metadata));
 
             return Ok(response);
         }
--- SocialMedia.Core/Services/PostService.cs
+++ SocialMedia.Core/Services/PostService.cs
@@ -4,7 +4,6 @@ using SocialMedia.Core.Exceptions;
 using SocialMedia.Core.Interfaces;
 using SocialMedia.Core.QueryFilters;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
