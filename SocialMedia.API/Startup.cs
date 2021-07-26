e16f4b04dc53ea3635025f38ebd82b0b89092c43
Mitchell Rodríguez <mitrodle@gmail.com>
2021-07-26 10:02:14 -0500

    [Add]: Local ISS

--- SocialMedia.API/Startup.cs
+++ SocialMedia.API/Startup.cs
@@ -120,8 +120,8 @@ namespace SocialMedia.API
             app.UseSwagger();
             app.UseSwaggerUI(options =>
             {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Social Media API V1");
-                options.RoutePrefix = string.Empty;
+                options.SwaggerEndpoint("../swagger/v1/swagger.json", "Social Media API V1");
+                //options.RoutePrefix = string.Empty;
             });
 
             app.UseRouting();
